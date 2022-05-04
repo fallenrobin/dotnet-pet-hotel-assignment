56a94e88e286a2ca3ce7ff9b5c1a0085393781a7
Andrew Lounsbury <lounsburya@gmail.com>
2022-05-04 16:47:59 -0500

    create PetOwner aand Pet models

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,15 +12,32 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
-            return new List<PetOwner>();
+        public IEnumerable<PetOwner> GetOwners()
+        {
+            return _context.PetOwners;
         }
+        [HttpGet("{id}")]
+        public ActionResult<PetOwner> GetById(int id)
+        {
+            PetOwner petOwner = _context.PetOwners
+            .SingleOrDefault(petOwner => petOwner.id == id);
+            if (petOwner == null)
+            {
+                return NotFound();
+            }
+            return petOwner;
+        }
+
+
+
+
     }
 }
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,46 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+
+    public enum PetBreedType
+    {
+        Shepherd,
+        Poodle,
+        Beagle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever
+    }
+    public enum PetColorType
+    {
+        White,
+        Black,
+        Golden,
+        Tricolor,
+        Spotted
+    }
+    public class Pet
+    {
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public PetBreedType breed { get; set; }
+
+        public PetColorType color { get; set; }
+
+        public DateTime checkedInAt { get; set; }
+
+        [ForeignKey("petOwner")]
+        public int petOwner_id { get; set; }
+        
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetOwner petOwner { get; set; }
+
+
+    }
+
 }
